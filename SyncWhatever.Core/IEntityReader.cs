a3f7678f2508787e1fe840a083806ff67366de28
Kaspars Ozols <kaspars@geta.no>
2016-06-18 11:38:30 +0300

    Defined main interfaces

---
+++ SyncWhatever.Core/IEntityReader.cs
@@ -0,0 +1,15 @@
+﻿namespace SyncWhatever.Core
+{
+    /// <summary>
+    /// Provides read access to entity
+    /// </summary>
+    public interface IEntityReader<out TEntity>
+    {
+        /// <summary>
+        /// Provides entity by entity key
+        /// </summary>
+        /// <param name="entityKey"></param>
+        /// <returns></returns>
+        TEntity GetEntity(string entityKey);
+    }
+}
---
+++ SyncWhatever.Core/ISyncTaskConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace SyncWhatever.Core
+{
+    /// <summary>
+    /// Provides configuration to synchronization task
+    /// </summary>
+    public interface ISyncTaskConfig<TSourceEntity, TTargetEntity>
+    {
+        ISyncStateReader<TSourceEntity> SourceSyncStateReader { get; set; }
+        IEntityReader<TSourceEntity> SourceReader { get; set; }
+        IEntityWriter<TSourceEntity> SourceWriter { get; set; }
+
+
+        ISyncStateReader<TTargetEntity> TargetSyncStateReader { get; set; }
+        IEntityReader<TTargetEntity> TargetReader { get; set; }
+        IEntityWriter<TTargetEntity> TargetWriter { get; set; }
+    }
+}
