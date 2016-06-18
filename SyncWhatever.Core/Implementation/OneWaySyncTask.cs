7774c3225e62ec0399bba19b91128cb22640def3
Kaspars Ozols <kaspars@geta.no>
2016-06-19 00:01:54 +0300

    Work in progress: sync task implementation

--- SyncWhatever.Core/Implementation/OneWaySyncTask.cs
+++ SyncWhatever.Core/Implementation/OneWaySyncTask.cs
@@ -8,7 +8,7 @@ namespace SyncWhatever.Core.Implementation
     public class OneWaySyncTask<TSourceEntity, TTargetEntity> : ISyncTask
     {
         private readonly IOneWaySyncTaskConfig<TSourceEntity, TTargetEntity> _config;
-        private SyncStateChangeDetector _syncStateChangeDetector;
+        private readonly SyncStateChangeDetector _syncStateChangeDetector;
 
         public OneWaySyncTask(IOneWaySyncTaskConfig<TSourceEntity, TTargetEntity> config)
         {
@@ -19,14 +19,23 @@ namespace SyncWhatever.Core.Implementation
         public void Execute()
         {
             // get state changes
-            var iterations = GetStateChanges();
+            var changes = GetStateChanges();
 
-            foreach(var iteration in iterations)
+            foreach (var change in changes)
             {
+                var iteration = new SyncIteration<TSourceEntity, TTargetEntity>();
 
+                // load source item
+                iteration.SourceEntityKey = ResolveSourceKey(iteration);
+                iteration.SourceEntity = ReadSourceEntity(iteration);
 
-                // load source and target items
+                //load target item
+                iteration.TargetEntityKey = ResolveTargetKey(iteration);
+                iteration.TargetEntity = ReadTargetEntity(iteration);
+
                 // detect data operation
+                iteration.Operation = DetectDataOperation(iteration);
+
                 // perform mapping
                 // store results
                 // store sync state
@@ -39,6 +48,63 @@ namespace SyncWhatever.Core.Implementation
             var lastStates = _config.LastSourceSyncStateReader.GetAllStates();
             var currentStates = _config.SourceSyncStateReader.GetAllStates();
             return _syncStateChangeDetector.DetectChanges(lastStates, currentStates);
+        }
+
+
+        private string ResolveSourceKey(SyncIteration<TSourceEntity, TTargetEntity> iteration)
+        {
+            return iteration.CurrentSyncState?.EntityKey ?? iteration.LastSyncState?.EntityKey;
+        }
+
+        private string ResolveTargetKey(SyncIteration<TSourceEntity, TTargetEntity> iteration)
+        {
+            if (iteration.SourceEntityKey == null)
+                return null;
+
+            //stateChange.SyncKeyMap = SyncKeyMapStorage.GetBySourceKey(Context, stateChange.SourceKey);
+
+            // stateChange.SyncKeyMap?.TargetKey;
+
+            //TODO: implement fallback
+            //if (iteration.SourceEntity != null && _config.tar)
+            //{
+            //    iteration.TargetEntity = _config.TargetReader.ReadEntity(iteration.SourceEntity);
+            //}
+            //TODO: implements
+            return null;
+        }
+
+        private TSourceEntity ReadSourceEntity(SyncIteration<TSourceEntity, TTargetEntity> iteration)
+        {
+            if (iteration.SourceEntityKey == null)
+                return default(TSourceEntity);
+
+            return _config.SourceReader.ReadEntity(iteration.SourceEntityKey);
+        }
+
+        private TTargetEntity ReadTargetEntity(SyncIteration<TSourceEntity, TTargetEntity> iteration)
+        {
+            if (iteration.TargetEntityKey == null)
+                return default(TTargetEntity);
+
+            return _config.TargetReader.ReadEntity(iteration.TargetEntityKey);
+        }
+
+        private OperationEnum DetectDataOperation(SyncIteration<TSourceEntity, TTargetEntity> iteration)
+        {
+            if (iteration.SourceEntity != null && iteration.TargetEntity == null)
+            {
+                return OperationEnum.Create;
+            }
+            if (iteration.SourceEntity != null && iteration.TargetEntity != null)
+            {
+                return OperationEnum.Update;
+            }
+            if (iteration.SourceEntity == null && iteration.TargetEntity != null)
+            {
+                return OperationEnum.Delete;
+            }
+            return OperationEnum.None;
         }
     }
 }
