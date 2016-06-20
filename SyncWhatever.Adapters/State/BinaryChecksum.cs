f5c6f72869a5a96d73d86108ff792c4c92fdef7d
Kaspars Ozols <kaspars@geta.no>
2016-06-20 07:25:35 +0300

    Added binary checksum calculator

---
+++ SyncWhatever.Adapters/State/BinaryChecksum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using SyncWhatever.Core.Interfaces;
+
+namespace SyncWhatever.Components.State
+{
+    public class BinaryChecksum : ISyncState
+    {
+        private BinaryChecksum()
+        {
+        }
+
+        public static BinaryChecksum Calculate(object value, object key)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, value);
+                var objectBytes = stream.ToArray();
+
+                var md5 = MD5.Create();
+                var hashBytes = md5.ComputeHash(objectBytes);
+
+                return new BinaryChecksum
+                {
+                    EntityKey = key.ToString(),
+                    EntityState = BitConverter.ToString(hashBytes)
+                };
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"EntityKey = '{EntityKey}', EntityState = '{EntityState}'";
+        }
+
+        public string EntityKey { get; set; }
+        public string EntityState { get; set; }
+    }
+}
